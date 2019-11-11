d56a586e79180c7a58479773c1da35bb04dfba2b
xueweijie <xueweijie@xiaomi.com>
2019-11-11 10:35:52 +0800

    finish heap practice

---
+++ src/main/java/GraphPractice.java
@@ -0,0 +1,23 @@
+/**
+ * @author xueweijie
+ * @date 19-11-8 下午5:33
+ */
+public class GraphPractice {
+
+    public class AdjacencyMatrix{
+
+    }
+
+    public class AdjacencyList{
+
+    }
+
+    public void breadthFirstSearch(){
+
+    }
+
+    public void depthFirstSearch(){
+
+    }
+
+}
